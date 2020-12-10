2f42447b0276c3efdf7f20ef5b3b80e2522ba815
miguelslc <k_miguel7@hotmail.com>
2020-12-10 20:13:20 -0300

    20201210

--- routes/api/nodemailer.js
+++ routes/api/nodemailer.js
@@ -11,6 +11,8 @@ router.post('/send/contactUs', (req, res, next) => {
         host: "smtp.gmail.com", //replace with your email provider
         port: 587,
         auth: {
+            user: "AlumniaUNLZ@gmail.com", // generated ethereal user
+            pass: "R3d0bl@nt3", // generated ethereal password
         },
     });
     
@@ -57,7 +59,9 @@ router.post('/send/certificadoExamen', (req, res, next) => {
         host: "smtp.gmail.com", //replace with your email provider
         port: 587,
         auth: {
-                    },
+            user: "AlumniaUNLZ@gmail.com", // generated ethereal user
+            pass: "R3d0bl@nt3", // generated ethereal password
+        },
     });
     
     // verify connection configuration
